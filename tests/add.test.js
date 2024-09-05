1d3f050927742588eef0f31723e7853a6a3c6361
mihirjain0041 <mihirjain0041@gmail.com>
2024-09-05 22:54:51 +0530

    Added tests for edge cases in add function

--- tests/add.test.js
+++ tests/add.test.js
@@ -3,3 +3,11 @@ const add = require('../src/add');
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
+
+test('adds -1 + 2 to equal 1', () => {
+    expect(add(-1, 2)).toBe(1);
+});
+
+test('adds 100 + 200 to equal 300', () => {
+    expect(add(100, 200)).toBe(300);
+});
